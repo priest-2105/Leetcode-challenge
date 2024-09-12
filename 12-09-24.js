7e8978d300d673be02d11458b4b3a9600195d420
Bailey <priest2105@gmail.com>
2024-09-12 19:26:54 +0100

    12-09-24 complete ✔✔

--- 12-09-24.js
+++ 12-09-24.js
@@ -1,1 +1,37 @@
- 
+
+
+
+
+
+// SOLUTION 
+
+let number = 5;
+
+function createCounter (init) {
+
+    init = number;
+
+    return {
+        increament : () => {
+            return init;
+        },
+
+        decreament : () => {
+            init--
+            return init;
+        },
+
+        reset : () => {
+            init;
+            return init;            
+        }
+    }
+
+}
+
+
+
+createCounter(5);
+createCounter(5).increament
+createCounter(5).decreament
+createCounter(5).reset

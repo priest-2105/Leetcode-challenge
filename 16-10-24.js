23677aa62f2ff5b7a1a0c661dbfcb58a74819a46
priest-2105 <priest2105@gmail.com>
2024-10-16 07:08:41 +0100

    16-10-24

---
+++ 16-10-24.js
@@ -0,0 +1,27 @@
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumOperations = function(nums) {
+  
+    let sum = 0;
+  
+    for (let i = 0; i < nums.length; i++ ){  
+       if (nums[i] % 3 != 0){
+        sum++
+       }
+    }   
+    return sum;
+
+    // switch (nums){
+    //     case: nums % 3 === 0,
+    //     sum += nums
+    //     break;
+    //     default  nums % 3 != 0,       
+    //     continue
+    // }
+    // return sum
+
+};

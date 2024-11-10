f8217de65047791827a7b7fa3fe2a449a3dd5e5a
priest-2105 <priest2105@gmail.com>
2024-11-10 01:33:32 +0100

    10-11-24

--- 26-09-24.js
+++ 26-09-24.js
@@ -7,11 +7,7 @@
 // A word is a maximal 
 // substring
 //  consisting of non-space characters only.
-
- 
-
 // Example 1:
-
 // Input: s = "Hello World"
 // Output: 5
 // Explanation: The last word is "World" with length 5.

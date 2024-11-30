669747d0e25b748204c1f645a8c93a252bc56f87
priest-2105 <priest2105@gmail.com>
2024-11-30 11:34:01 +0100

    commit

--- 26-09-24.js
+++ 26-09-24.js
@@ -5,10 +5,6 @@
 // Given a string s consisting of words and spaces, return the length of the last word in the string.
 
 
-
-
-// A word is a maximal 
-
 // substring
 //  consisting of non-space characters only.
 // Example 1:
